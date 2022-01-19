2a02b94fe1c18a790a2621bc2540f40697a91555
Pooja <96231234+poojas2003@users.noreply.github.com>
2022-01-19 05:01:47 +0000

    enter my name

---
+++ p1original.c
@@ -0,0 +1,24 @@
+//add two numbers using 4 functions using pass by value
+
+#include<stdio.h> 
+void input(int *a,int *b)    //function definition
+{
+printf("enter two number\n");
+scanf("%d%d",a,b);
+}
+void add(int a,int b,int *sum)    //function definition
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("%d+%d=%d\n",a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  input(&a,&b);            //pass by reference
+  add(a,b,&sum);           //pass by value(a,b) and reference(sum)
+  output(a,b,sum);         //pass by value
+  return 0;
+}
