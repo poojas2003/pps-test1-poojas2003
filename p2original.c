c05cf31f63c55c0469a710c6132f75102b0ee8dd
Pooja <96231234+poojas2003@users.noreply.github.com>
2022-01-30 11:42:40 +0000

    add two number using 4 function using pass by value

--- p2original.c
+++ p2original.c
@@ -1,1 +1,26 @@
-
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter value of a\n");
+  scanf("%d",&a);
+  return a;
+}
+int add(int a,int b)
+{
+  int sum;
+  sum=a+b;
+  return sum;
+}
+void output(int a,int b,int sum)
+{
+  printf("sum of %d + %d is %d",a,b,sum);
+}
+  int main()
+{
+int a=input();
+int b=input();
+int sum=add(a,b);
+output(a,b,sum);
+return 0;
+}
